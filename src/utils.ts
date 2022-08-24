b11d33a80561dbd66d3f55bcc3dc8c78e2fd655d
julong <julong1988@naver.com>
2022-08-24 23:03:03 +0900

    fix: formatPath 오류 수정

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,6 @@ import { sync } from 'glob';
 import { filesPattern, ignore } from './config';
 
 // 포맷 가져오기
-export const formatPath = (name: string): string => resolve(__dirname, `../node_modules/eslint/lib/cli-engine/formatters/${name}`);
+export const formatPath = (name: string): string => resolve(process.cwd(), `./node_modules/eslint/lib/cli-engine/formatters/${name}`);
 // ignore 추가
 export const getLintFileList = (): string[] => sync(filesPattern, { ignore });
